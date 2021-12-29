04761bb4eed3f6ff7ddb807a07081cc818a8a9e0
iregina <reginawong14@gmail.com>
2021-12-29 03:37:24 -0800

    publish solution and learnings for day 1

--- day1/day1.js
+++ day1/day1.js
@@ -1,4 +1,4 @@
-// The following program extracts the contents of the 'depths.txt' file
+// The following program contains an async function that extracts the contents of the 'depths.txt' file. Source: https://stackoverflow.com/questions/66973034/how-to-set-variable-value-inside-read-file-function-and-use-it-outside-in-node-j
 
 // console.log("hello the program is starting now");
 
@@ -10,3 +10,39 @@
 // });
 
 
+// Objective: Figure out how quickly the depth increases - the number of times a depth measurement increases
+
+import fs, { readFileSync } from 'fs';
+
+var count = 0;
+var depths = fs.readFileSync('depths.txt').toString().split("\n");
+// console.log(depths);
+for (let i = 0; i < depths.length; i++) {
+	if ((depths[i+1] < depths[i])&&(Number(depths[i+1])>Number(depths[i]))){
+		console.log(i);
+		console.log(depths[i+1] + " is less than " + depths[i] )
+		count++;
+	} 
+}
+console.log(count);
+
+
+var depths2 = fs.readFileSync('depths.txt')
+		.toString()
+		.split('\n')
+		.map(n => parseInt(n, 10));
+	// console.log(depths2);
+	let count2 = 0;
+
+	for (let i = 0; i < depths2.length; i++) {
+		if (i > 0 && depths2[i] > depths2[i-1]) {
+			count2++
+		} 
+	}
+
+	console.log(count2); // 1st answer
+
+
+
+
+	// TIL "10" is not greater than "9" in JavaScript
