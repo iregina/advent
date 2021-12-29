35c1fcc5d5b2546876353e652c40c20fd6baf317
iregina <reginawong14@gmail.com>
2021-12-29 01:09:56 -0800

    extract content of the depth.txt file

---
+++ day1/day1.js
@@ -0,0 +1,12 @@
+// The following program extracts the contents of the 'depths.txt' file
+
+// console.log("hello the program is starting now");
+
+// import fs, { readFile } from 'fs';
+
+// readFile('depths.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
