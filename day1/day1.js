1a766aa17997eff3c523ca23ae838cced2aa5b2b
iregina <reginawong14@gmail.com>
2021-12-30 01:48:52 -0800

    clean up code

--- day1/day1.js
+++ day1/day1.js
@@ -9,7 +9,6 @@
 //     console.log(data);
 // });
 
-
 // Objective: Figure out how quickly the depth increases - the number of times a depth measurement increases
 
 import fs, { readFileSync } from 'fs';
@@ -17,32 +16,14 @@ import fs, { readFileSync } from 'fs';
 var count = 0;
 var depths = fs.readFileSync('depths.txt').toString().split("\n");
 // console.log(depths);
+
 for (let i = 0; i < depths.length; i++) {
-	if ((depths[i+1] < depths[i])&&(Number(depths[i+1])>Number(depths[i]))){
-		console.log(i);
-		console.log(depths[i+1] + " is less than " + depths[i] )
+	var previous_depth = Number(depths[i]);
+	var current_depth = Number(depths[i+1]);
+	if (current_depth > previous_depth){
 		count++;
 	} 
 }
-console.log(count);
+console.log("The depth measurement increases " + count + " times.");
 
-
-var depths2 = fs.readFileSync('depths.txt')
-		.toString()
-		.split('\n')
-		.map(n => parseInt(n, 10));
-	// console.log(depths2);
-	let count2 = 0;
-
-	for (let i = 0; i < depths2.length; i++) {
-		if (i > 0 && depths2[i] > depths2[i-1]) {
-			count2++
-		} 
-	}
-
-	console.log(count2); // 1st answer
-
-
-
-
-	// TIL "10" is not greater than "9" in JavaScript
+// TIL "10" is not greater than "9" in JavaScript (no type declaration)
